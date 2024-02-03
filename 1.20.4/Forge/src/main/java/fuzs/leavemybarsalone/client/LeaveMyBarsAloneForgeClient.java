1272b0963c12d7c323dff64cdc12696661bd8004
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-03 22:39:59 +0100

    prepare 1.20.4 port

---
+++ 1.20.4/Forge/src/main/java/fuzs/leavemybarsalone/client/LeaveMyBarsAloneForgeClient.java
@@ -0,0 +1,39 @@
+package fuzs.leavemybarsalone.client;
+
+import fuzs.leavemybarsalone.LeaveMyBarsAlone;
+import fuzs.leavemybarsalone.client.handler.RidingBarsHandler;
+import fuzs.leavemybarsalone.integration.appleskin.AppleSkinIntegration;
+import fuzs.puzzleslib.api.core.v1.ModLoaderEnvironment;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+
+@Mod.EventBusSubscriber(modid = LeaveMyBarsAlone.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class LeaveMyBarsAloneForgeClient {
+
+    @SubscribeEvent
+    public static void onConstructMod(final FMLConstructModEvent evt) {
+        registerHandlers();
+        registerIntegration();
+    }
+
+    private static void registerHandlers() {
+        MinecraftForge.EVENT_BUS.addListener(RidingBarsHandler::onRenderGuiOverlayEvent$Pre);
+    }
+
+    private static void registerIntegration() {
+        if (ModLoaderEnvironment.INSTANCE.isModLoaded("appleskin")) {
+            AppleSkinIntegration.init();
+        }
+    }
+
+    @SubscribeEvent
+    public static void onRegisterGuiOverlays(final RegisterGuiOverlaysEvent evt) {
+        evt.registerAbove(VanillaGuiOverlay.FOOD_LEVEL.id(), "food_level_mounted", RidingBarsHandler.FOOD_LEVEL_MOUNTED_GUI_OVERLAY);
+        evt.registerAbove(VanillaGuiOverlay.EXPERIENCE_BAR.id(), "experience_bar_mounted", RidingBarsHandler.EXPERIENCE_BAR_MOUNTED_GUI_OVERLAY);
+    }
+}
---
+++ 1.20.4/Forge/src/main/java/fuzs/leavemybarsalone/integration/appleskin/AppleSkinIntegration.java
@@ -0,0 +1,69 @@
+package fuzs.leavemybarsalone.integration.appleskin;
+
+import fuzs.leavemybarsalone.LeaveMyBarsAlone;
+import fuzs.leavemybarsalone.client.handler.RidingBarsHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
+import net.minecraftforge.common.MinecraftForge;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
+
+public class AppleSkinIntegration {
+
+    public static void init() {
+        MethodHandle renderExhaustionHandle;
+        MethodHandle renderFoodOrHealthOverlayHandle;
+        Object foodOverlayType;
+        try {
+            Class<?> renderOverlayTypeClazz = Class.forName("squeek.appleskin.client.HUDOverlayHandler$RenderOverlayType");
+            MethodType renderExhaustionType = MethodType.methodType(void.class, ForgeGui.class, GuiGraphics.class, float.class, int.class, int.class);
+            MethodType renderFoodOrHealthOverlayType = MethodType.methodType(void.class, ForgeGui.class, GuiGraphics.class, float.class, int.class, int.class, renderOverlayTypeClazz);
+            Class<?> clazz = Class.forName("squeek.appleskin.client.HUDOverlayHandler");
+            renderExhaustionHandle = MethodHandles.publicLookup().findStatic(clazz, "renderExhaustion", renderExhaustionType);
+            renderFoodOrHealthOverlayHandle = MethodHandles.publicLookup().findStatic(clazz, "renderFoodOrHealthOverlay", renderFoodOrHealthOverlayType);
+            Field foodField = renderOverlayTypeClazz.getField("FOOD");
+            foodField.setAccessible(true);
+            MethodHandle foodGetter = MethodHandles.publicLookup().unreflectGetter(foodField);
+            foodOverlayType = foodGetter.invoke();
+        } catch (Throwable e) {
+            LeaveMyBarsAlone.LOGGER.warn("Failed to initialize Apple Skin integration", e);
+            return;
+        }
+        MinecraftForge.EVENT_BUS.addListener((RenderGuiOverlayEvent.Pre evt) -> onRenderGuiOverlay$Pre(evt, renderExhaustionHandle));
+        MinecraftForge.EVENT_BUS.addListener((RenderGuiOverlayEvent.Post evt) -> onRenderGuiOverlay$Post(evt, renderFoodOrHealthOverlayHandle, foodOverlayType));
+    }
+
+    private static void onRenderGuiOverlay$Pre(final RenderGuiOverlayEvent.Pre evt, MethodHandle methodHandle) {
+        if (evt.getOverlay().overlay() == RidingBarsHandler.FOOD_LEVEL_MOUNTED_GUI_OVERLAY) {
+            Minecraft minecraft = Minecraft.getInstance();
+            ForgeGui gui = (ForgeGui) minecraft.gui;
+            if (minecraft.player.getVehicle() instanceof LivingEntity && !minecraft.options.hideGui && gui.shouldDrawSurvivalElements()) {
+                try {
+                    methodHandle.invoke(gui, evt.getGuiGraphics(), evt.getPartialTick(), evt.getWindow().getScreenWidth(), evt.getWindow().getScreenHeight());
+                } catch (Throwable e) {
+                    LeaveMyBarsAlone.LOGGER.warn("Failed to render Apple Skin exhaustion overlay", e);
+                }
+            }
+        }
+    }
+
+    private static void onRenderGuiOverlay$Post(final RenderGuiOverlayEvent.Post evt, MethodHandle methodHandle, Object foodOverlayType) {
+        if (evt.getOverlay().overlay() == RidingBarsHandler.EXPERIENCE_BAR_MOUNTED_GUI_OVERLAY) {
+            Minecraft minecraft = Minecraft.getInstance();
+            ForgeGui gui = (ForgeGui) minecraft.gui;
+            if (minecraft.player.getVehicle() instanceof LivingEntity && !minecraft.options.hideGui && gui.shouldDrawSurvivalElements()) {
+                try {
+                    methodHandle.invoke(gui, evt.getGuiGraphics(), evt.getPartialTick(), evt.getWindow().getScreenWidth(), evt.getWindow().getScreenHeight(), foodOverlayType);
+                } catch (Throwable e) {
+                    LeaveMyBarsAlone.LOGGER.warn("Failed to render Apple Skin food overlay", e);
+                }
+            }
+        }
+    }
+}
